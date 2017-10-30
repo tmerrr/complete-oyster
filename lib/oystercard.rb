572bdfb8cf16d5344315e9d6e1cdb83c4aeddba2
Peter Johnstone <peter.wdj@gmail.com>
2017-10-30 17:08:59 +0000

    Added passing test to limit maximum Oystercard balance to £90

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :maximum_amount
+  MAXIMUM_AMOUNT = 90
 
   def initialize
     @balance = 0
+    @maximum_amount = MAXIMUM_AMOUNT
   end
 
   def top_up(amount)
+    fail "Sorry, the balance on your Oyster card can not exceed #{MAXIMUM_AMOUNT}." if (@balance + amount) > MAXIMUM_AMOUNT
     @balance += amount
   end
 end
