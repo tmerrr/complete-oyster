0dc57208199011e6aec2bf4fdc8218d5be53909b
Jamie LeMercier <jamielemercier@gmail.com>
2017-10-30 16:30:23 +0000

    Adds passing test for #top_up in oystercard spec

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,13 @@
 require 'oystercard'
 
 describe Oystercard do
-
-  it "Initialise a new Oystercard with a balance of 0" do
+  it 'Initialise a new Oystercard with a balance of 0' do
     expect(subject.balance).to eq 0
   end
-  
+  describe '#top_up' do
+    it 'tops up oyster card balance with £20' do
+      subject.top_up(20)
+      expect(subject.balance).to eq(20)
+    end
+  end
 end
